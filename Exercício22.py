2240c1c05e367f346c75e75d28aaee980b61e0ec
BRCMOURA <brcmoura@gmail.com>
2024-10-16 19:01:30 -0300

    Add files via upload

---
+++ Exercício22.py
@@ -0,0 +1,11 @@
+#Divisão inteira de dois números sem considerar o resto
+
+num1 = int(input("Digite um número:"))
+
+num2 = int(input("Digite outro número:"))
+
+divisao = num1 // num2
+
+print(f'A divisão inteira de {num1} por {num2} é: {divisao}')
+
+
